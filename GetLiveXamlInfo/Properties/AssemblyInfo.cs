d3a00b146122081a5c1d1f3873009d6ef0d1ee07
Matt Lacey <matt@mrlacey.co.uk>
2019-06-09 16:20:40 +0100

    tidying up

--- GetLiveXamlInfo/Properties/AssemblyInfo.cs
+++ GetLiveXamlInfo/Properties/AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GetLiveXamlInfo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GetLiveXamlInfo")]
+[assembly: AssemblyCompany("Matt Lacey")]
+[assembly: AssemblyProduct("GetLiveXamlInfo © Matt Lacey 2019")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
